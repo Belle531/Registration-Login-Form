15ad1b8055715f1dba9fc08d1d93d6fefaa194a3
Cassandra <cassandra.moore1516@gmail.com>
2025-11-12 13:52:15 -0600

    allowedHosts: ['cds-ys0t.onrender.com']

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174
+    port: 5174,
+    preview: {
+      allowedHosts: ['cds-ys0t.onrender.com']
+    }
   }
 })
